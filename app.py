eb5bb14af118cbaf532857d25d4abd3ee49eb03e
alanchn31 <alanchn31@gmail.com>
2019-01-08 22:31:41 +0800

    First commit Flower App

---
+++ app.py
@@ -0,0 +1,53 @@
+import os
+from flask import Flask, request, render_template, flash, \
+    redirect, url_for
+
+from utils import *
+
+import torch
+
+app = Flask(__name__)
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+UPLOAD_FOLDER = 'images'
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('base.html', title='Flower App')
+
+@app.route('/upload', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+       if 'file' not in request.files:
+           flash('No file part')
+           return redirect(request.url)
+       file = request.files['file']
+       if file.filename == '':
+           flash('No selected file')
+           return redirect(request.url)
+       if file and allowed_file(file.filename):
+           file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+           file.save(file_path)
+           return redirect(url_for('.uploaded', file_path=file_path))
+
+@app.route('/uploaded')
+def uploaded():
+    file_path = request.args['file_path']
+    model_path = 'model/udacity_proj_densenet_121_model.pt'
+    model = torch.load(model_path,map_location='cpu')
+    plot_bar(file_path, model)
+    return redirect(url_for('result'))
+
+@app.route('/result')
+def result():
+    return render_template('plot.html', title='Flower App')
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ utils.py
@@ -0,0 +1,114 @@
+import torch
+import torch.nn.functional as F
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+import json
+
+with open('data/cat_to_name.json', 'r') as f:
+    cat_to_name = json.load(f)
+
+def process_image(image):
+    ''' Scales, crops, and normalizes a PIL image for a PyTorch model,
+        returns an Numpy array
+    '''
+
+    # Resize where shortest side is 256px, keeping aspect ratio
+    minside = 256
+    img = Image.open(image)
+    imagex, imagey = img.size
+    aspect = float(imagex) / float(imagey)
+
+    if imagex <= imagey:
+        width = 256
+        height = int(width / aspect)
+    else:
+        height = 256
+        width = int(height * aspect)
+
+    img = img.resize((width, height), Image.ANTIALIAS)
+
+    # Crop out center 224 x 224
+    left = (width - 224) / 2
+    top = (height - 224) / 2
+    right = (width + 224) / 2
+    bottom = (height + 224) / 2
+    img = img.crop((left, top, right, bottom))
+
+    # Convert image to numpy array
+    np_image = np.array(img)
+    np_image = np_image / 255
+
+    # Normalize image
+    np_image -= [0.485, 0.456, 0.406]
+    np_image /= [0.229, 0.224, 0.225]
+
+    # Transpose array:
+    result = np_image.transpose(-1, 0, 1)
+
+    return result
+
+
+def imshow(image, ax=None, title=None):
+    """Imshow for Tensor."""
+    if ax is None:
+        fig, ax = plt.subplots()
+
+    # PyTorch tensors assume the color channel is the first dimension
+    # but matplotlib assumes is the third dimension
+    image = image.numpy().transpose((1, 2, 0))
+
+    # Undo preprocessing
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    image = std * image + mean
+
+    # Image needs to be clipped between 0 and 1 or it looks like noise when displayed
+    image = np.clip(image, 0, 1)
+
+    ax.imshow(image)
+
+    return ax
+
+
+def predict(image_path, model, topk=5, train_on_gpu=False):
+    ''' Predict the class (or classes) of an image using a trained deep learning model.
+    '''
+
+    # TODO: Implement the code to predict the class from an image file
+    model.eval()
+    image = process_image(image_path)
+    image = torch.from_numpy(image)
+    if train_on_gpu:
+        image = image.cuda()
+    image = image.float().unsqueeze(0)
+    out = model.forward(image)
+    logps = F.log_softmax(out)
+    ps = torch.exp(logps)
+    probs, classes = ps.topk(5, dim=1)
+    if train_on_gpu:
+        probs = list(probs.squeeze(0).cpu().detach().numpy())
+        classes = list(classes.squeeze(0).cpu().detach().numpy())
+    else:
+        probs = list(probs.squeeze(0).detach().numpy())
+        classes = list(classes.squeeze(0).detach().numpy())
+    idx_class_mapping = dict((v, k) for k, v in model.class_to_idx.items())
+    classes = list(map(lambda x: idx_class_mapping[x], classes))
+    return probs, classes
+
+def plot_bar(image_path, model):
+    result = process_image(image_path)
+    res = torch.from_numpy(result)
+    fig, (ax1, ax2) = plt.subplots(figsize=(6,9), ncols=2)
+    ax1 = imshow(res, ax1)
+    probs, classes = predict(image_path,model)
+    ax2.barh(np.arange(len(probs)), probs)
+    ax2.set_aspect(0.1)
+    ax2.set_yticks(np.arange(len(probs)))
+    classes = list(map(lambda x: cat_to_name[x], classes))
+    ax2.set_yticklabels(classes, size='small');
+    ax2.set_title('Class Probability')
+    ax2.set_xlim(0, 1.1)
+    plt.tight_layout()
+    plt.savefig('static/plot_bar.png',bbox_inches='tight',pad_inches = 0.0)
+
