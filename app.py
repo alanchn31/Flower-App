f28d2bfd99d1b71833aae86b9ce65d86773d159b
alanchn31 <alanchn31@gmail.com>
2019-10-07 20:51:50 +0800

    Add no-caching to response headers

--- app.py
+++ app.py
@@ -30,6 +30,16 @@ def allowed_file(filename):
 def index():
     return render_template('base.html', title='Flower App')
 
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=0'
+    return response
+
 @app.route('/upload', methods = ['GET', 'POST'])
 def upload_file():
    if request.method == 'POST':
